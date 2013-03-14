f995c393f56bd88a16714142e86f03a1ef9f65c2
Assen Kolov <assen.kolov@gmail.com>
2013-03-14 09:23:16 +0100

    wip

--- src-cljs/stairs/stairs.cljs
+++ src-cljs/stairs/stairs.cljs
@@ -8,7 +8,7 @@
             [clojure.string :as str]
             [clojure.browser.repl :as repl])
   (:require-macros [tarapana.csutil :as csutil])
-  (:use [jayq.core :only [$ css inner]]
+  (:use ;[jayq.core :only [$ css inner]]
         [jayq.util :only [map->js]]
         [domina :only [append!]]
         [domina.xpath :only [xpath]]
@@ -30,9 +30,24 @@
 (defn ^:export initPage []   (
                   do
                   (repl/connect "http://localhost:9000/repl")
-                   (append! (sel "#stairs-input") "<div>Hello world!</div>")
+                 ;  (append! (sel "#stairs-input") "<div>Repl started</div>")
                 ))
 
+(defn floor [level]
+  (str "<div class=\"row\">"
+       "<div class=\"span2\"><input class=\"small-input\" id=\"stream" level "-1\" value=\"100\"> --&gt;</div>"
+       "<div class=\"span2\"></div>"
+       "<div class=\"span2\"> &lt;-- <input class=\"small-input\" id=\"stream" level "-2\" value=\"100\"> </div>"
+       "</div>"
+       "<div class=\"row\">"
+    "<div class=\"span2\"></div>"
+    "<div class=\"span2\"><input class=\"small-input\" id=\"stream" level "-3\" value=\"15\"></div>"
+    "</div>"
+    ))
+
+(defn draw-stair[el n]
+  (append! el (reduce str (map floor (reverse (range n))) )) )
+
 ; (set! (.-onload js/window) onload)
-(set! (.-onclick generateButton) #(js/alert "click"))
+(set! (.-onclick generateButton) #(draw-stair (sel "#stairs-input") 5))
 
